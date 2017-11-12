bf82d5e8b644f0ec3b1885405ee42208b16ae6d3
mikuadam <mikulas.adam@apertus.uni-nke.hu>
2017-11-12 18:46:55 +0100

    add iphone/ipad fallback

--- src/js/app.js
+++ src/js/app.js
@@ -4,28 +4,40 @@ import { SPEAKERS } from './data';
 
 // Wait till domcontentloaded
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // Check if it is a mobile, tablet or any touch device due to Skewed feature
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        document.getElementById('wrapperForMobile').style.display = 'block';
+    } else {
+        document.getElementById('wrapper').style.display = 'block';        
+        // Implement SKEW feature
+        const wrapper = document.getElementById('wrapper');
+        const topLayer = wrapper.querySelector('.top');
+        const handle = wrapper.querySelector('.animLine');
+        let skew = 0;
+        let delta = 0;
 
-    // Implement SKEW feature
-    const wrapper = document.getElementById('wrapper');
-    const topLayer = wrapper.querySelector('.top');
-    const handle = wrapper.querySelector('.animLine');
-    let skew = 0;
-    let delta = 0;
-
-    if (wrapper.className.indexOf('skewed') != -1) {
-        skew = 2000;
+        if (wrapper.className.indexOf('skewed') != -1) {
+            skew = 2000;
+        }
+        // Add mousemove eventlistenes to the wrapper tag and set the proper values
+        wrapper.addEventListener('mousemove', (e) => {
+            delta = (e.clientX - window.innerWidth / 2) * 0.5;
+            handle.style.left = e.clientX + delta + 'px';
+            topLayer.style.width = e.clientX + skew + delta + 'px';
+        });
     }
-    // Add mousemove eventlistenes to the wrapper tag and set the proper values
-    wrapper.addEventListener('mousemove', (e) => {
-        delta = (e.clientX - window.innerWidth / 2) * 0.5;
-        handle.style.left = e.clientX + delta + 'px';
-        topLayer.style.width = e.clientX + skew + delta + 'px';
-    });
+
+    // Check if it is an Iphone or Ipad or Ipod due to autoplay video
+    if ( /iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+        document.getElementById('video').style.display = 'none';   
+        document.getElementById('fallbackBackground').style.display = 'block !important';        
+    }
 
     // Add sticky button feature, show and hide
     window.onscroll = () => scrollFunction()
 
-    function scrollFunction() {
+    let scrollFunction = () => {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             document.getElementById("fixedBtn").style.display = "block";
         } else {
@@ -33,6 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+
+    // Fill speakers section from source
     let speakersHTML = '';
     SPEAKERS.forEach(person => {
         speakersHTML += `
@@ -46,4 +60,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }) 
 
     document.querySelector('.speakers ul').innerHTML = speakersHTML;
+
 });
